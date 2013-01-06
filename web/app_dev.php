6c41a964b8b6d34a84144f9944a2eff49cf5d0a7
Iván Guillén <zeopix@gmail.com>
2013-01-06 03:55:11 +0100

    Update web/app_dev.php

--- web/app_dev.php
+++ web/app_dev.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Request;
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
+/*
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || !in_array(@$_SERVER['REMOTE_ADDR'], array(
@@ -18,6 +19,8 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
+*/
+//this is a test version, so dev mode is allowed by default!
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 require_once __DIR__.'/../app/AppKernel.php';
